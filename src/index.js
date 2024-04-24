dfb36106f21fa495b75328563a170fa8f798ccff
HuntsberryS <112701256+HuntsberryS@users.noreply.github.com>
2024-04-24 16:56:06 -0400

    UX changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function getRecipe(event) {
 
   let apiKey = "7036a98t226cf2o3c044afd3b96a58b7";
   let cuisineChoice = document.querySelector("#cuisine");
-  let prompt = `generate a simple ${cuisineChoice.value} recipe. Please make sure to include required ingredients at the beginning of the recipe. Please do not include any special characters aside from punctuation. Add <br /> <br /> before writing the recipe.`;
+  let prompt = `generate a simple ${cuisineChoice.value} recipe. Please make sure to include required ingredients at the beginning of the recipe. Be sure to include the recipe name first. Please do not include any special characters aside from punctuation. Add <br /> <br /> before writing the recipe.`;
   let context =
     "You are a wholesome home cook who loves sharing easy recipes with people. Please provide simple, easy to follow responses that are concise";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
