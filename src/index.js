b43da06b1f33cd8b565d6858de90400eefe24709
HuntsberryS <huntsberry.shiloh@gmail.com>
2024-10-28 08:55:10 -0400

    style update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ function getRecipe(event) {
 
   let apiKey = "7036a98t226cf2o3c044afd3b96a58b7";
   let cuisineChoice = document.querySelector("#cuisine");
-  let prompt = `generate a simple ${cuisineChoice.value} recipe. Please make sure to include required ingredients at the beginning of the recipe. Be sure to include the recipe name first. Please do not include any special characters aside from punctuation. Add <br /> <br /> before writing the recipe.`;
+  let prompt = `generate a simple ${cuisineChoice.value} recipe with the recipe name included at the top, followed by a list of ingredients required and then the steps to make the meal. The test should have no special characters in it and follow thhis output: Recipe name <br/> <br/> Ingredients: (list of ingredients numbered) <br/> <br/>. Instructions: (steps to prepare meal) numbered. `;
   let context =
-    "You are a wholesome home cook who loves sharing easy recipes with people. Please provide simple, easy to follow responses that are concise";
+    "You are a wholesome home cook who loves sharing easy recipes with people. Please provide simple, easy to follow responses and include recipes that may not be well known.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(generateRecipe);
